6aba61184b5ddedfe669da12e5577c97478f6ad3
David Rodríguez <david.rodriguez@est.fi.uncoma.edu.ar>
2020-08-31 17:01:08 -0300

    Se crea la clase PingPong del ejemplo de la clase teorica numero 1.

---
+++ PingPong.java
@@ -0,0 +1,35 @@
+
+
+public class PingPong extends Thread{
+    private String cadena; //Lo que va a escribir
+    private int delay; // Tiempo entre escritura
+
+    public PingPong(String cartel, int cantMs) {
+        cadena = cartel;
+        delay = cantMs;
+    }
+    public void run(){
+        for(int i = 1; i<delay * 10; i++){
+            System.out.println(cadena+" ");
+            try {Thread.sleep(delay);                
+            } catch (InterruptedException e) {
+            }
+        }
+    }//fin metodo run().
+//fin clase PingPong.
+
+public static void main(String[] args) {
+    PingPong t1 = new PingPong("PING",33);
+    PingPong t2 = new PingPong("PONG",10);
+
+    //Activación
+    t1.start();
+    t2.start();
+
+    //Espera unos segundos
+    try{Thread.sleep(5000);
+    }catch (InterruptedException e){};
+
+    //Finaliza la ejecución de los threads.
+}
+}
