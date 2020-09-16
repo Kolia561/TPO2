c43e0915baf0915b3841356a34542663dab8c5d6
David Rodríguez <david.rodriguez@est.fi.uncoma.edu.ar>
2020-09-15 21:57:36 -0300

    Se corrige el dato compartido para ser del tipo adecuado

--- EjemplosTeoria/Estacionamiento/EstacionamientoAuto.java
+++ EjemplosTeoria/Estacionamiento/EstacionamientoAuto.java
@@ -1,7 +1,7 @@
 package EjemplosTeoria.Estacionamiento;
 
 public class EstacionamientoAuto extends Estacionamiento {
-    private Object cantAutos;
+    private int cantAutos;
     private final int maxAutos = 10;
 
     public EstacionamientoAuto(){
@@ -9,21 +9,16 @@ public class EstacionamientoAuto extends Estacionamiento {
     }
 
     public synchronized void ingresarAuto() {
-        int aux;
-        if ((int)cantAutos<maxAutos) {
-            aux = (int) cantAutos;
-            cantAutos = aux+1;
-            System.out.println("Ingresa un auto, hay "+(int)cantAutos+" de un maximo de: "+maxAutos);
-        }
-        else{System.out.println("ESTACIONAMIENTO LLENO, SE VA UN AUTO SIN PODER INGRESAR");}
+        if (cantAutos<maxAutos) {
+            cantAutos = cantAutos+1;
+            System.out.println("Ingresa un auto, hay "+ cantAutos+" de un maximo de: "+maxAutos);
+        }else{System.out.println("ESTACIONAMIENTO LLENO, SE VA UN AUTO SIN PODER INGRESAR");}
     }
 
     public synchronized void egresarAuto(){
-        if ((int) cantAutos>0) {
-            int aux;
-            aux = (int) cantAutos;
-            cantAutos = aux-1;
-            System.out.println("Sale un auto, hay "+(int)cantAutos+" de un maximo de: "+maxAutos);
+        if (cantAutos>0) {
+            cantAutos = cantAutos-1;
+            System.out.println("Sale un auto, hay "+ cantAutos+" de un maximo de: "+maxAutos);
         } else {System.out.println("ESTACIONAMIENTO DE AUTOS VACIO");}
     }
 
--- EjemplosTeoria/Estacionamiento/EstacionamientoMoto.java
+++ EjemplosTeoria/Estacionamiento/EstacionamientoMoto.java
@@ -1,7 +1,7 @@
 package EjemplosTeoria.Estacionamiento;
 
 public class EstacionamientoMoto extends Estacionamiento{
-    private Object cantMotos;
+    private int cantMotos;
     private final int maxMotos = 10;
 
     public EstacionamientoMoto(){
@@ -9,20 +9,16 @@ public class EstacionamientoMoto extends Estacionamiento{
     }
     
     public synchronized void ingresarMoto() {
-        int aux;
-        if ((int)cantMotos<maxMotos) {
-            aux = (int) cantMotos;
-            cantMotos = aux+1;
-            System.out.println("Ingresa una moto, hay "+(int)cantMotos+" de un maximo de: "+maxMotos);
+        if (cantMotos<maxMotos) {
+            cantMotos = cantMotos+1;
+            System.out.println("Ingresa una moto, hay "+cantMotos+" de un maximo de: "+maxMotos);
         }else{System.out.println("ESTACIONAMIENTO LLENO, SE VA UNA MOTO SIN PODER INGTRESAR");}
     }
     
     public synchronized void egresarMoto(){
-        if ((int)cantMotos>0) {
-            int aux;
-            aux = (int) cantMotos;
-            cantMotos = aux-1;
-            System.out.println("Sale una moto, hay "+(int)cantMotos+" de un maximo de: "+maxMotos);
+        if (cantMotos>0) {
+            cantMotos = cantMotos-1;
+            System.out.println("Sale una moto, hay "+cantMotos+" de un maximo de: "+maxMotos);
         }else {System.out.println("ESTACIONAMIENTO DE MOTOS VACIO");}
     }    
 }
