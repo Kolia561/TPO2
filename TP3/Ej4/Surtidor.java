b885c239b24f809fe092b9a999e8c88d8d4a432c
David Rodríguez <david.rodriguez@est.fi.uncoma.edu.ar>
2020-09-18 17:14:08 -0300

    modificacion sobre que se sincroniza, en lugar del surtidor sincronizo el auto que es el objeto al que se le modifica la variable

--- TP3/Ej4/Surtidor.java
+++ TP3/Ej4/Surtidor.java
@@ -1,10 +1,12 @@
 package TP3.Ej4;
 
 public class Surtidor {
-    public synchronized void surtir(Auto unAuto){
+    public void surtir(Auto unAuto){
+        synchronized(unAuto) {
         System.out.println("se cargara combustible en: "+Thread.currentThread().getName());
         unAuto.cargarCombustible();
         System.out.println("se cargo combustible en: "+Thread.currentThread().getName());
+        }
     }
 
 }
