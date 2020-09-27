333040c6349b0c5ff0465c3543da2a8de0b87e2c
Kolia561 <nicolas.blanco@est.fi.uncoma.edu.ar>
2020-09-27 20:33:11 -0300

    Comentarios

--- src/tpo2/Ejercicio6/Recurso.java
+++ src/tpo2/Ejercicio6/Recurso.java
@@ -17,46 +17,72 @@ import java.util.concurrent.Semaphore;
  */
 public class Recurso {
 
-    private Semaphore semA= new Semaphore(1);
-    private Semaphore semB= new Semaphore(0);
-    private Semaphore semC= new Semaphore(0);
-    private String letra;
-    private int veces;
-    private String nombreT;
+    /** Los semaforos que se van a encargar de manejar
+     * que letra corresponde imprimir.
+     * El primer semaforo se inicia con un permiso, porque
+     * se necesita que por algun lado comience a escribir
+    */
+    private Semaphore semA = new Semaphore(1);
+    private Semaphore semB = new Semaphore(0);
+    private Semaphore semC = new Semaphore(0);
+    
+
+    /**Creo un HasMap para poder asociar las letras con los semaforos que
+     * daran paso a la escritura.
+     *  Utilizo List como Value porque la documentacion de oracle lo hace asi
+     * no se si con un simple array se podria hacer pero por las dudas lo 
+     * segui de la misma forma
+     */
     private Map<Character, List<Semaphore>> m = new HashMap<Character, List<Semaphore>>();
-    private List<Semaphore> lista = new ArrayList<Semaphore>();
+    
+    /**Como use List, tuve que crear una para cada key porque no estaba seguro si al
+     *  pasarcelas al HashMap lo hace por referencia o por valor.
+     */
+    private List<Semaphore> lista1 = new ArrayList<Semaphore>();
     private List<Semaphore> lista2 = new ArrayList<Semaphore>();
     private List<Semaphore> lista3 = new ArrayList<Semaphore>();
 
     public Recurso() {
-
         
-        lista.add(semA);
-        lista.add(semB);
+        //Lleno cada lista con sus respectivos semaforos
+        lista1.add(semA);
+        lista1.add(semB);
         lista2.add(semB);
         lista2.add(semC);
         lista3.add(semC);
         lista3.add(semA);
-        
-        this.m.put('a', lista);
+
+        //Y se las asocia a cada letra(key)
+        this.m.put('a', lista1);
         this.m.put('b', lista2);
         this.m.put('c', lista3);
 
-
     }
 
-
-
-
-
-    public void escribir(Character letra , int times) {
-
-        List m;
+    public boolean escribir(Character letra, int times) throws InterruptedException {
+        /** Este es el metodo que se encargaria de imprimir las letras.
+         * Va a recibir por parametro que tiene que imprimir (Character letra)
+         * y lo va a imprimir (int times) haga falta.
+         * Si la letra no esta en el HashMap devuelve false
+         */
+        boolean rta= false;
+        List<Semaphore> listaux;
         
+        listaux = m.get(letra);
 
-       
+        if (listaux != null) {
+            
+            rta=!rta;
+            listaux.get(0).acquire();   //Pide por el tura para ecribir
+            
+            for (int i = 0; i < times; i++) {
+                System.out.print(letra);
+            }
+            
+            listaux.get(1).release(); //Le cede el turno a la siguiente "letra"
 
+        }
+        return rta;
     }
 
-    
 }
